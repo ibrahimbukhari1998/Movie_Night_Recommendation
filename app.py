d0dacb87c77f645f763d02c70fa25a201d36b852
ibrahimbukhari19 <ibrahimbukhari1998@gmail.com>
2025-02-12 18:54:20 +0100

    Added Full Movie List

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
 
         # Compute cosine similarity
         similarities = cosine_similarity(user_vector, movie_vectors)[0]
-        top_movies = sorted(zip(movies_metadata.keys(), similarities), key=lambda x: x[1], reverse=True)[:7]
+        top_movies = sorted(zip(movies_metadata.keys(), similarities), key=lambda x: x[1], reverse=True)[:10]
 
         st.subheader("🎥 Your Recommended Movies:")
         for movie, score in top_movies:
